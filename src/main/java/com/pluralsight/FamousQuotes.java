90f9879d1c61e92932433644b190e7466bf2e775
Julian Palmgren <julianpalmgren@gmail.com>
2025-10-06 11:24:37 -0400

    Second commit, added menu options allowing the user to select a quote, see a random quote, or exit. Also added question prompting the user if they want to see another quote.

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -26,13 +26,48 @@ public class FamousQuotes {
 
                 System.out.println("=====[Famous Quotes in IT]=====");
                 System.out.println("");
-                System.out.print("Enter a number 1-10 for a famous quote: ");
-                int quoteIndex = sc.nextInt();
+                System.out.println("(1) Select a quote to read");
+                System.out.println("(2) Read a random quote");
+                System.out.println("(3) Exit");
+                System.out.print("Enter a number 1-3 for menu options: ");
+                int menuIndex = sc.nextInt();
                 sc.nextLine();
 
-                System.out.println(quotes[quoteIndex - 1]);
+                switch(menuIndex) {
+                    case 1:
+                        System.out.println("=====[Quote Selection Menu]=====");
+                        System.out.println("");
+                        System.out.print("Enter a number 1-10 for a famous quote: ");
+                        int quoteIndex = sc.nextInt();
+                        sc.nextLine();
 
-                quoteSelect = false;
+                        System.out.println(quotes[quoteIndex - 1]);
+
+                        System.out.println("");
+                        System.out.println("Would you like to read another quote?");
+                        System.out.println("(1) Yes");
+                        System.out.println("(2) No");
+                        int anotherQuote = sc.nextInt();
+                        sc.nextLine();
+
+                        if (anotherQuote == 1) {
+                            quoteSelect = true;
+                        } else {
+                            quoteSelect = false;
+                        }
+
+                        break;
+                    case 2:
+                        int randomIndex = (int) (Math.random() * quotes.length);
+                        String randomQuote = quotes[randomIndex];
+                        System.out.println(randomQuote);
+
+
+                        break;
+                    case 3:
+                        quoteSelect = false;
+                        break;
+                }
 
             } catch (Exception e) {
                 System.out.println(e.getMessage());
@@ -40,6 +75,5 @@ public class FamousQuotes {
                 System.out.println("Please try again!\n");
             }
         }
-
     }
 }
